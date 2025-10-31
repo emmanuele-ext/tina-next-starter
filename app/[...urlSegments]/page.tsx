1e3f1b13a22ffb34f1cb0f66da0c835c74fd4ec8
Emmanuele Durante <emmanuele.durante@extera.com>
2025-10-31 15:28:57 +0100

    make the projeect sr

--- app/[...urlSegments]/page.tsx
+++ app/[...urlSegments]/page.tsx
@@ -5,7 +5,7 @@ import Layout from '@/components/layout/layout';
 import { Section } from '@/components/layout/section';
 import ClientPage from './client-page';
 
-export const revalidate = 300;
+export const dynamic = 'force-dynamic';
 
 export default async function Page({
   params,
@@ -31,34 +31,4 @@ export default async function Page({
       </Section>
     </Layout>
   );
-}
-
-export async function generateStaticParams() {
-  let pages = await client.queries.pageConnection();
-  const allPages = pages;
-
-  if (!allPages.data.pageConnection.edges) {
-    return [];
-  }
-
-  while (pages.data.pageConnection.pageInfo.hasNextPage) {
-    pages = await client.queries.pageConnection({
-      after: pages.data.pageConnection.pageInfo.endCursor,
-    });
-
-    if (!pages.data.pageConnection.edges) {
-      break;
-    }
-
-    allPages.data.pageConnection.edges.push(...pages.data.pageConnection.edges);
-  }
-
-  const params = allPages.data?.pageConnection.edges
-    .map((edge) => ({
-      urlSegments: edge?.node?._sys.breadcrumbs || [],
-    }))
-    .filter((x) => x.urlSegments.length >= 1)
-    .filter((x) => !x.urlSegments.every((x) => x === 'home')); // exclude the home page
-
-  return params;
 }
--- app/posts/[...urlSegments]/page.tsx
+++ app/posts/[...urlSegments]/page.tsx
@@ -3,7 +3,7 @@ import client from '@/tina/__generated__/client';
 import Layout from '@/components/layout/layout';
 import PostClientPage from './client-page';
 
-export const revalidate = 300;
+export const dynamic = 'force-dynamic';
 
 export default async function PostPage({
   params,
@@ -21,32 +21,4 @@ export default async function PostPage({
       <PostClientPage {...data} />
     </Layout>
   );
-}
-
-export async function generateStaticParams() {
-  let posts = await client.queries.postConnection();
-  const allPosts = posts;
-
-  if (!allPosts.data.postConnection.edges) {
-    return [];
-  }
-
-  while (posts.data?.postConnection.pageInfo.hasNextPage) {
-    posts = await client.queries.postConnection({
-      after: posts.data.postConnection.pageInfo.endCursor,
-    });
-
-    if (!posts.data.postConnection.edges) {
-      break;
-    }
-
-    allPosts.data.postConnection.edges.push(...posts.data.postConnection.edges);
-  }
-
-  const params =
-    allPosts.data?.postConnection.edges.map((edge) => ({
-      urlSegments: edge?.node?._sys.breadcrumbs,
-    })) || [];
-
-  return params;
 }
